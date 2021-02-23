79e026c8189fb080fd43b7021ec359ef54fa9fd7
tarkerin <feelthegamesense@gmail.com>
2021-02-23 16:26:13 -0300

    ascii script

---
+++ script.py
@@ -0,0 +1,49 @@
+import PIL.Image
+
+# ascii characters que vao ser usados
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+# Resize na imagem
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height/width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return(resized_image)
+
+# converter pixels em grayscale
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+    
+# converter pixels em ascii
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)    
+
+def main(new_width=100):
+    # mensagens ao rodar o script
+    path = input("Coloque um pathname valido:\n")
+    try:
+        image = PIL.Image.open(path)
+    except:
+        print(path, " Pathname usado nao e valido.")
+        return
+  
+    # converter a imagem   
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+    
+    # format
+    pixel_count = len(new_image_data)  
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
+    
+    # print resultado
+    print(ascii_image)
+    
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+ 
+# rodar
+main()
